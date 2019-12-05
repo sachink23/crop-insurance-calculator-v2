739288a671e89e242f29e8937769c36cbf674c12
Sachin Kekarjawalekar <kekarjawalekarsachin1@gmail.com>
2019-12-05 21:02:58 +0530

    new update to service worker

--- src/app/services/update/pwa-update.service.ts
+++ src/app/services/update/pwa-update.service.ts
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 @Injectable()
 export class PwaUpdateService {
   constructor(private swUpdate: SwUpdate) {
+    console.log("Welcome to https://www.kportal.in, Crop Insurance Calculator by Kekarjawalekar Technologies")
     this.swUpdate.available.subscribe(evt => {
         Swal.fire({
           title: "Update Available",
@@ -22,6 +23,7 @@ export class PwaUpdateService {
           }
         })
     })
+    
   }
   update() {
     window.location.reload(true)
