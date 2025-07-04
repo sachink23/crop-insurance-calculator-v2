bb6bf294a36ec0800ad7753c2b6a4f4af64e01a4
Sachin Kekarjawalekar <sachin@kekarjawalekar.com>
2025-07-04 17:52:07 +0530

    Added Jalana District - Kharip 2025

--- src/app/data/seasons/seasons.ts
+++ src/app/data/seasons/seasons.ts
@@ -104,6 +104,100 @@ export class Seasons {
                 headquarter_address: 'Office Block-1, 5th Floor, Plate-B & C, East Kidwai Nagar, Ring Road, New Delhi-110023',
                 headquarter_address_mr: ''
               }
+            },
+            {
+              district_id: '514',
+              district_key: '9AF33CF1-BC0B-4AE2-98A5-3E908B29F38D',
+              district_name: 'Jalna',
+              district_name_mr: '\u091c\u093e\u0932\u0928\u093e',
+              crops: [
+                {
+                  crop_key: '9662A6FC-0329-4553-9094-7B4F37BBB794',
+                  crop_name: 'Black Gram (Urad)',
+                  crop_name_mr: 'उडीद',
+                  crop_category: 'Pulses',
+                  insurance_pricing: {
+                    premium_farmer_share: 55,
+                    premium_gov_share: 0,
+                    sum_insured: 22000
+                  }
+                },
+                {
+                  crop_key: 'A77E13E5-ABE3-4C56-A8E4-97DA16F2ACD1',
+                  crop_name: 'Cotton (Kapas)',
+                  crop_name_mr: 'कापूस',
+                  crop_category: 'Fiber Crops',
+                  insurance_pricing: {
+                    premium_farmer_share: 900,
+                    premium_gov_share: 0,
+                    sum_insured: 60000
+                  }
+                },
+                {
+                  crop_key: '776D349D-7644-4899-8813-53FB0CEF466D',
+                  crop_name: 'Green Gram (Moong)',
+                  crop_name_mr: 'मुग',
+                  crop_category: 'Pulses',
+                  insurance_pricing: {
+                    premium_farmer_share: 560,
+                    premium_gov_share: 6440,
+                    sum_insured: 28000
+                  }
+                },
+                {
+                  "crop_key": "875E34A6-DCA3-4214-90D7-A9ABF0373B6B",
+                  "crop_name": "Maize (Makka)",
+                  "crop_name_mr": "\u092e\u0915\u093e",
+                  "crop_category": "Millets",
+                  "insurance_pricing": {
+                    "premium_farmer_share": 180,
+                    "premium_gov_share": 0,
+                    "sum_insured": 36000
+                  }
+                },
+                {
+                  crop_key: '6EBA253F-5001-436D-A73E-95551CEF7C25',
+                  crop_name: 'Pearl Millet (Bajra)',
+                  crop_name_mr: 'बाजरी',
+                  crop_category: 'Millets',
+                  insurance_pricing: {
+                    premium_farmer_share: 80,
+                    premium_gov_share: 0,
+                    sum_insured: 32000
+                  }
+                },
+                {
+                  crop_key: '4BF7051B-1F38-4DEC-8929-7DBEBADFE505',
+                  crop_name: 'Pigeon Pea (Red Gram/Arhar/Tur)',
+                  crop_name_mr: 'तूर',
+                  crop_category: 'Pulses',
+                  insurance_pricing: {
+                    premium_farmer_share: 470,
+                    premium_gov_share: 0,
+                    sum_insured: 47000
+                  }
+                },
+                {
+                  crop_key: 'AAC11C1E-0F72-4495-80FD-ECB4A4051DAC',
+                  crop_name: 'Soybean (Bhat)',
+                  crop_name_mr: 'सोयाबीन',
+                  crop_category: 'Oilseeds',
+                  insurance_pricing: {
+                    premium_farmer_share: 1160,
+                    premium_gov_share: 8120,
+                    sum_insured: 58000
+                  }
+                }
+              ],
+              insurance_company_details: {
+                name: 'AGRICULTURE INSURANCE COMPANY',
+                name_mr: '',
+                toll_free_number: '14447',
+                email: '',
+                website: '',
+                headquarter_address: 'Office Block-1, 5th Floor, Plate-B & C, East Kidwai Nagar, Ring Road, New Delhi-110023',
+                headquarter_address_mr: ''
+              }
             }
           ]
         }
